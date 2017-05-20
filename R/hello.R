d209fdc99c05582aa83e89324d85c93d46c14978
Jose Brito <jambrito@gmail.com>
2017-05-20 01:19:31 +0000

    version 1.1

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,38 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+optimal_allocation<-function(Nh_Vh,n,LL,minimumsample,X)
+{
+  Nh=Nh_Vh[1:LL]
+  Vh=Nh_Vh[((LL+1):(2*LL))]
+  funcao_objetivo=as.vector(outer(1/c(1:n),(Vh*Nh^2)))
+  inf=1
+  sup=n
+  for(i in 1:LL) {funcao_objetivo[inf:sup]=funcao_objetivo[inf:sup]-(Vh[i]*Nh[i]);inf=sup+1;sup=sup+n}
+  funcao_objetivo[which(funcao_objetivo<0)]=max(Vh)*max(Nh^2)*10
+  restricoes=c(rep("==",LL+1),rep("<=",LL),rep(">=",LL))
 
-hello <- function() {
-  print("Hello, world!")
+  b=c(rep(1,LL),n,Nh,rep(minimumsample,LL))
+  A=matrix(0,nrow=(3*LL)+1,ncol=n*LL)
+  for(i in 1:LL) {A[i,(1+(n*(i-1))):(i*n)]=rep(1,n)}
+  A[LL+1,]=rep(c(1:n),LL)
+  inf=1
+  sup=n
+  for(i in (LL+2):((2*LL)+1)){A[i,inf:sup]=c(1:n);inf=sup+1;sup=sup+n}
+  for(i in ((2*LL)+2):((3*LL)+1)) {A[i,]=A[i-LL,]}
+  solucao=Rglpk_solve_LP(funcao_objetivo,A,restricoes,b,types=rep("B",length(funcao_objetivo)),max=FALSE)
+  status=solucao$status
+  var_tot=solucao$optimum
+  solucao=solucao$solution
+  x=matrix(rep(0,LL),nrow=1,byrow=TRUE)
+  x[1,1]=sum(solucao[1:n]*c(1:n))
+  if (LL>1)
+  {inf=n+1
+  sup=2*n
+  for(i in 2:LL)
+  {x[1,i]=sum(solucao[inf:sup]*c(1:n))
+  inf=sup+1
+  sup=sup+n
+  }
+  }
+  if (status>0) {var_tot=Inf}
+  solution=matrix(c(x,sqrt(var_tot)/X),nrow=1)
+  return(solution)
 }
--- R/stratbr.R
+++ R/stratbr.R
@@ -1,15 +1,16 @@
 #'Optimization Algorithm to solve stratification problem
 #'
-#'@param X  Stratification variable.
-#'@param stratas  Number of strata.
-#'@param nsample  Sample size.
-#'@param tampop   Number of chromosomes BRKGA.
-#'@param totgen   Number of generations BRKGA.
-#'@param pelite   Percentage elite solutions BRKGA.
-#'@param pmutant  Percentage mutant solutions BRKGA.
-#'@param rc       Crossover probability  BRKGA.
-#'@param minimumsample Minimum sample size (smallest possible sample size in any stratum).
-#'@param cores Numerical amount of CPUs requested for the cluster.
+#'@param X             Stratification variable.
+#'@param H             Number of strata.
+#'@param n             Sample size.
+#'@param nmin          Minimum sample size (smallest possible sample size in any stratum).
+#'@param takeall       Take-all stratum (takeall=TRUE) =>  nH=NH.
+#'@param tampop        Number of chromosomes BRKGA.
+#'@param totgen        Number of generations BRKGA.
+#'@param pelite        Percentage elite solutions BRKGA.
+#'@param pmutant       Percentage mutant solutions BRKGA.
+#'@param rc            Crossover probability  BRKGA.
+#'@param cores         Numerical amount of CPUs requested for the cluster.
 #'@return \item{cvtot}{Coefficient of variation for the estimator of total of the stratification variable considered.}
 #'@return \item{nh}{Number of sample elements, or sample size, in stratum h.}
 #'@return \item{Nh}{Number of population elements, or population size, in stratum h.}
@@ -33,7 +34,7 @@
 #'
 #'@description This function aims at constructing optimal strata with an optimization algorithm
 #'based on a global optimisation technique called Biased Random Key Genetic Algorithms(BRKGA).
-#'The optimization algorithm is applied to solve the onedimensional case,
+#'The optimization algorithm is applied to solve the one dimensional case,
 #'which reduces the stratification problem to just determining strata boundaries.
 #'Assuming that the number H of strata and the total sample size n are fixed,
 #'it is possible to produce the strata boundaries by taking into consideration
@@ -51,13 +52,12 @@
 #'@examples
 #'data(Sweden)
 #'REV84<-Sweden[,9]
-#'solution1<-stratbr(REV84,stratas=3,nsample=100,cores=4,totgen=20)
-#'#'solution2<-stratbr(REV84,stratas=3,nsample=100,cores=4,totgen=20,minimumsample=30)
-#'ME84<-Sweden[,8]
-#'solution3<-stratbr(ME84,stratas=4,nsample=75,cores=2,totgen=20)
+#'solution1<-stratbr(REV84,H=3,n=50,nmin=10,totgen=2,cores=4)
+#'data(USbanks)
+#'solution2<-stratbr(USbanks,H=3,n=50,totgen=2,cores=4,takeall=TRUE)
 
-stratbr<-function(X,stratas=3,nsample=30,tampop=100,totgen=1500,pelite=0.2,
-                  pmutant=0.3,rc=0.6,minimumsample=2,cores=2)
+stratbr<-function(X,H=3,n=30,nmin=2,takeall=FALSE,tampop=100,totgen=1500,pelite=0.2,
+                  pmutant=0.3,rc=0.6,cores=2)
 {
 read_procedure<-function(X,Xmin)
   {X=sort(X)
@@ -68,54 +68,15 @@ read_procedure<-function(X,Xmin)
     return(list(N=length(X),X=X,B=B,scale_mult=scale_div));
   }
 
-optimal_allocation<-function(Nh_Vh,n,LL,minimumsample,X)
-{
-  Nh=Nh_Vh[1:LL]
-  Vh=Nh_Vh[((LL+1):(2*LL))]
-  funcao_objetivo=as.vector(outer(1/c(1:n),(Vh*Nh^2)))
-  inf=1
-  sup=n
-  for(i in 1:LL) {funcao_objetivo[inf:sup]=funcao_objetivo[inf:sup]-(Vh[i]*Nh[i]);inf=sup+1;sup=sup+n}
-  funcao_objetivo[which(funcao_objetivo<0)]=max(Vh)*max(Nh^2)*10
-  restricoes=c(rep("==",LL+1),rep("<=",LL),rep(">=",LL))
 
-  b=c(rep(1,LL),n,Nh,rep(minimumsample,LL))
-  A=matrix(0,nrow=(3*LL)+1,ncol=n*LL)
-  for(i in 1:LL) {A[i,(1+(n*(i-1))):(i*n)]=rep(1,n)}
-  A[LL+1,]=rep(c(1:n),LL)
-  inf=1
-  sup=n
-  for(i in (LL+2):((2*LL)+1)){A[i,inf:sup]=c(1:n);inf=sup+1;sup=sup+n}
-  for(i in ((2*LL)+2):((3*LL)+1)) {A[i,]=A[i-LL,]}
-  solucao=Rglpk_solve_LP(funcao_objetivo,A,restricoes,b,types=rep("B",length(funcao_objetivo)),max=FALSE)
-  status=solucao$status
-  var_tot=solucao$optimum
-  solucao=solucao$solution
-  x=matrix(rep(0,LL),nrow=1,byrow=TRUE)
-  x[1,1]=sum(solucao[1:n]*c(1:n))
-  if (LL>1)
-  {inf=n+1
-  sup=2*n
-  for(i in 2:LL)
-  {x[1,i]=sum(solucao[inf:sup]*c(1:n))
-  inf=sup+1
-  sup=sup+n
-  }
-  }
-  if (status>0) {var_tot=Inf}
-  solution=matrix(c(x,sqrt(var_tot)/X),nrow=1)
-  return(solution)
-}
-
-
-  pop_gen<-function(psize,L,faixa)
+pop_gen<-function(psize,L,faixa)
   {
     popa<-cbind(matrix(runif(psize*(L-1),0,1),nrow=psize,ncol=L-1,byrow=TRUE),sample(faixa,psize,TRUE))
     return(popa)
   }
 
 
-  decoder<-function(pop,stratas,Nb,B,X,qtd)
+  decoder<-function(pop,H,Nb,B,X,qtd)
   {
     calcula_Vh_Nh<-function(x,X,H)
     {Vh<-rep(0,H)
@@ -129,18 +90,18 @@ optimal_allocation<-function(Nh_Vh,n,LL,minimumsample,X)
     return(cbind(Nh,Vh))
     }
     ak<-B[1]
-    decod<-t(apply(pop,1,function(x) sort(ak+x[1:(stratas-1)]*(qtd[x[stratas]]-ak))))
+    decod<-t(apply(pop,1,function(x) sort(ak+x[1:(H-1)]*(qtd[x[H]]-ak))))
+    if (H==2) {decod<-t(decod)}
     decod<-cbind(-Inf,decod,Inf)
-    Nh_Vh<-matrix(apply(decod,1,function(x) calcula_Vh_Nh(x,X,stratas)),ncol=2*stratas,byrow=TRUE)
+    Nh_Vh<-matrix(apply(decod,1,function(x) calcula_Vh_Nh(x,X,H)),ncol=2*H,byrow=TRUE)
     return(cbind(Nh_Vh,decod))
   }
 
 
 
   ##########Objective Function#########################################################################
-  fitness<-function(g,nsample,LL,minimumsample,X)
-  {
-    db<-t(snowfall::sfApply(g,1,function(g) optimal_allocation(g,nsample,LL,minimumsample,X)))
+  fitness<-function(g,n,LL,nmin,X,takeall)
+  { db<-t(snowfall::sfApply(g,1,function(g) BSSM_FD(g[1:LL],g[(LL+1):(2*LL)],n,LL,nmin,X,takeall=takeall)))
     return(db)
   }
 
@@ -181,19 +142,19 @@ optimal_allocation<-function(Nh_Vh,n,LL,minimumsample,X)
   pquantil<-ifelse(length(population$B)>100,0.01,0.1)
   if (length(population$B)>100) {faixa<-c(90:101)} else {faixa<-c(5:10)}
   qtd<-quantile(population$B,probs=seq(0,1,pquantil))
-  f<-pop_gen(psize=size_population,stratas,faixa)
+  f<-pop_gen(psize=size_population,H,faixa)
   tx<-sum(population$X)
-  g<-decoder(f,stratas,N,population$B,population$X,qtd)
-  ft<-fitness(g[,1:(2*stratas)],nsample,stratas,minimumsample,tx)
+  g<-decoder(f,H,N,population$B,population$X,qtd)
+  ft<-fitness(g[,1:(2*H)],n,H,nmin,tx,takeall)
   fbest<-Inf
   i<-0
   smelhoriag<-0
   while((i<=totgen) & (smelhoriag<500))
      {i<-i+1
       smelhoriag<-smelhoriag+1
-      pq<-order(ft[,(stratas+1)])
+      pq<-order(ft[,(H+1)])
       f<-f[pq,] #Ordena pela Fitness
-      fmin<-ft[pq[1],(stratas+1)]
+      fmin<-ft[pq[1],(H+1)]
       ft<-ft[pq,]
       g<-g[pq,]
       if (fmin<fbest)
@@ -207,18 +168,92 @@ optimal_allocation<-function(Nh_Vh,n,LL,minimumsample,X)
        }
   felite<-f[(1:pelite),] #Elite
   fnonelite<-f[(pelite+1):size_population,] #N?o Elite
-  fmutant<-pop_gen(psize=pmutant,stratas,faixa) #Mutantes
-  fnovos<-crossover_uniforme(felite,fnonelite,rc,pelite,(size_population-pelite),(size_population-pelite-pmutant),stratas)
+  fmutant<-pop_gen(psize=pmutant,H,faixa) #Mutantes
+  fnovos<-crossover_uniforme(felite,fnonelite,rc,pelite,(size_population-pelite),(size_population-pelite-pmutant),H)
   f<-rbind(fmutant,fnovos)
-  g<-rbind(g[1:pelite,],decoder(f,stratas,N,population$B,population$X,qtd))
+  g<-rbind(g[1:pelite,],decoder(f,H,N,population$B,population$X,qtd))
   f<-rbind(felite,f)
   glk<-g[(pelite+1):size_population,]
-  ftk<-fitness(glk[,1:(2*stratas)],nsample,stratas,minimumsample,tx)
+  ftk<-fitness(glk[,1:(2*H)],n,H,nmin,tx,takeall)
   ft<-rbind(ft[1:pelite,],ftk)
 
   }
   tempo<-(proc.time()-tempo)[3]
   snowfall::sfStop()
-  return(list(cvtot=round(100*fbest,5),nh=solution[1:stratas],Nh=gbest[1:stratas],Sh2=gbest[(stratas+1):(2*stratas)],bk=gbest[(2*stratas+2):(3*stratas)],cputime=tempo))
+  return(list(cvtot=round(100*fbest,5),nh=solution[1:H],Nh=gbest[1:H],Sh2=gbest[(H+1):(2*H)],bk=gbest[(2*H+2):(3*H)],cputime=tempo))
+}
 
+
+#'Optimal Allocation - Minimum Coefficient of Variation
+#'
+#'@param Nh            Vector with number of population elements, or population size, in stratum h
+#'@param Sh2x          Vector with population variance of the variable X in stratum h.
+#'@param n             Sample size.
+#'@param H             Number of strata.
+#'@param nmin          Minimum sample size (smallest possible sample size in any stratum).
+#'@param X             Population Total
+#'@param takeall       Take-all stratum (takeall=TRUE) =>  nH=NH.
+#'@return \item{solution}{Vector with  sample of size by stratum and coefficient of variation
+#'                        for the estimator of total of the stratification variable considered.}
+#'
+#'@references Brito, J.A.M, Silva, P.L.N.,Semaan, G.S. and Maculan, N. (2015).
+#'            Integer Programming Formulations Applied to Optimal Allocation in Stratified Sampling.
+#'            Survey Methodology, 41: 427-442.
+#'@author Jose Brito (jambrito@gmail.com), Pedro Silva, Gustavo Semaan and Nelson Maculan.
+#'
+#'@description Function that uses an integer programming formulation for allocation
+#'       of the overall sample size n to the strata,  for the following purpose:
+#'    Coefficient of Variation of the estimate of total for the survey variable is minimized.
+#'@export
+#'@examples
+#'X<-round(100*runif(50))
+#'Nh<-c(10,20,20)
+#'Sh2x<-c(var(X[1:10]),var(X[11:30]),var(X[31:50]))
+#'aloc1<-BSSM_FD(Nh,Sh2x,n=40,H=3,nmin=2,sum(X),takeall=TRUE)
+#'Nh<-c(49,78,20,39,73,82,89)
+#'X<-542350
+#'Sh2x<-c(4436978,5581445,33454902,5763294,8689167,3716130,13938505)
+#'aloc2<-BSSM_FD(Nh,Sh2x,n=100,H=7,nmin=2,X)
+
+BSSM_FD<-function(Nh,Sh2x,n,H,nmin,X,takeall=FALSE)
+{
+  Nh=Nh
+  Vh=Sh2x
+  funcao_objetivo=as.vector(outer(1/c(1:n),(Vh*Nh^2)))
+  inf=1
+  sup=n
+  for(i in 1:H) {funcao_objetivo[inf:sup]=funcao_objetivo[inf:sup]-(Vh[i]*Nh[i]);inf=sup+1;sup=sup+n}
+  funcao_objetivo[which(funcao_objetivo<0)]=max(Vh)*max(Nh^2)*10
+  restricoes=c(rep("==",H+1),rep("<=",H),rep(">=",H))
+
+  b=c(rep(1,H),n,Nh,rep(nmin,H))
+  A=matrix(0,nrow=(3*H)+1,ncol=n*H)
+  for(i in 1:H) {A[i,(1+(n*(i-1))):(i*n)]=rep(1,n)}
+  A[H+1,]=rep(c(1:n),H)
+  inf=1
+  sup=n
+  for(i in (H+2):((2*H)+1)){A[i,inf:sup]=c(1:n);inf=sup+1;sup=sup+n}
+  for(i in ((2*H)+2):((3*H)+1)) {A[i,]=A[i-H,]}
+  if (takeall==TRUE)
+  {restricoes[2*H+1]<-"=="}
+
+  solucao=Rglpk_solve_LP(funcao_objetivo,A,restricoes,b,types=rep("B",length(funcao_objetivo)),max=FALSE)
+  status=solucao$status
+  var_tot=solucao$optimum
+  solucao=solucao$solution
+  x=matrix(rep(0,H),nrow=1,byrow=TRUE)
+  x[1,1]=sum(solucao[1:n]*c(1:n))
+  if (H>1)
+  {inf=n+1
+  sup=2*n
+  for(i in 2:H)
+  {x[1,i]=sum(solucao[inf:sup]*c(1:n))
+  inf=sup+1
+  sup=sup+n
+  }
+  }
+
+  if (status>0) {var_tot=Inf}
+  solution=c(x,sqrt(var_tot)/X)
+  return(solution)
 }
